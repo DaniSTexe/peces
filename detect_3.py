e61377f4aa066d42e2f5bf8824ddd14ad74fec78
DaniSTexe <odos26@hotmail.com>
2022-04-07 21:27:55 -0500

    Simlator 2 Ok: Ordenado

--- detect_3.py
+++ detect_3.py
@@ -309,7 +309,7 @@ def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
 
 def parse_opt():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'colas1.pt', help='model path(s)')
+    parser.add_argument('--weights', nargs='+', type=str, default=ROOT / './weights/colas1.pt', help='model path(s)')
     parser.add_argument('--source', type=str, default=ROOT / './Inputs/Conteo_32', help='file/dir/URL/glob, 0 for webcam')
     parser.add_argument('--data', type=str, default=ROOT / 'data/coco128.yaml', help='(optional) dataset.yaml path')
     parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
